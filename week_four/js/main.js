53e94fc62f49d54b462a69dec04a4f2241f6ef0b
michaelanell <mickimaree@gmail.com>
2022-05-14 17:58:30 -0600

    added to submit js

--- week_four/js/main.js
+++ week_four/js/main.js
@@ -1,14 +1,4 @@
-const form = document.forms.search;
 const input = form.elements.searchInput;
 
 input.addEventListener('focus', () => alert('focused'), false);
-input.addEventListener('blur', () => alert('blurred'), false);
 input.addEventListener('change', () => alert('changed'), false);
-
-form.addEventListener ('submit', search, false);
-
-
-function search(event) {
-    alert('Form Submitted');
-    event.preventDefault();
-}
