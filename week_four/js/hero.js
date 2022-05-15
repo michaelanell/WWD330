7bb4c566477c0b907117e853ce48f0da6463699b
michaelanell <mickimaree@gmail.com>
2022-05-14 20:48:01 -0600

    hero js updates

--- week_four/js/hero.js
+++ week_four/js/hero.js
@@ -3,16 +3,14 @@ form.addEventListener('submit', makeHero, false);
 
 function makeHero(event) {
 
-    event.preventDefault(); // prevent the form from being submitted
 
     const hero = {}; // create an empty object
 
-    hero.name = form.heroName.value; // create a name property based on the input field's value
-
-    alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
-    
-    //catch form values: 
-
+      //catch form values: 
+    hero.category = form.category.value;
+    hero.age = form.age.value;
+    hero.city = form.city.value;
+    hero.origin = form.origin.value;
     hero.realName = form.realName.value;
     hero.powers = [];
     for (let i=0; i < form.powers.length; i++) {
@@ -20,12 +18,9 @@ function makeHero(event) {
         hero.powers.push(form.powers[i].value);
     }
 }
-hero.category = form.category.value;
-hero.age = form.age.value;
-hero.city = form.city.value;
-hero.origin = form.origin.value;
+event.preventDefault(); // prevent the form from being submitted
 
-
+alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
 return hero;
 }
 
