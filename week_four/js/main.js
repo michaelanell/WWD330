d55247e36b600da0309cb5107d8b47236b48b768
michaelanell <mickimaree@gmail.com>
2022-05-14 18:33:52 -0600

    fixed search js

--- week_four/js/main.js
+++ week_four/js/main.js
@@ -1,13 +1,25 @@
-//
+// Code for search.html
+const form = document.forms['search'];
 const input = form.elements.searchInput;
 
-//input.addEventListener('focus', () => alert('focused'), false);
-//input.addEventListener('change', () => alert('changed'), false);
-
-const form = document.forms['search'];
 form.addEventListener ('submit', search, false);
 
-function search() {
+function search(event) {
     alert(`You Searched for: ${input.value}`);
-    Event.preventDefault();
+    event.preventDefault();
 }
+
+input.value = 'Search Here';
+
+input.addEventListener('focus', function(){
+    if (input.value==='Search Here') {
+        input.value = '' 
+    }
+}, false);
+
+input.addEventListener('blur', function(){
+    if(input.value === '') {
+        input.value = 'Search Here';
+    } }, false);
+
+    
