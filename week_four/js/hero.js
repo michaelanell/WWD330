6e9fa3bba0ce955b0fbd931352aab5e31013216e
michaelanell <mickimaree@gmail.com>
2022-05-14 20:54:28 -0600

    hero validation changes

--- week_four/js/hero.js
+++ week_four/js/hero.js
@@ -1,5 +1,10 @@
 const form = document.forms['hero'];
+
+// event listeners
 form.addEventListener('submit', makeHero, false);
+form.addEventListener('submit',validate,false);
+form.heroName.addEventListener('keyup',disableSubmit,false);
+
 
 function makeHero(event) {
 
@@ -24,3 +29,35 @@ alert(JSON.stringify(hero)); // convert object to JSON string and display in ale
 return hero;
 }
 
+
+
+function validate(event) {
+    const firstLetter = form.heroName.value[0];
+    if (firstLetter.toUpperCase() === 'X') {
+        event.preventDefault();
+        alert('Your name is not allowed to start with X!');
+    }
+}
+
+const label = form.querySelector('label');
+const error = document.createElement('div');
+error.classList.add('error');
+error.textContent = '! Your name is not allowed to start with X.';
+label.append(error);
+
+function validateInline() {
+    const heroName = this.value.toUpperCase();
+    if(heroName.startsWith('X')){
+    error.style.display = 'block';
+    } else {
+    error.style.display = 'none';
+    }
+}
+
+function disableSubmit(event) {
+    if(event.target.value === ''){
+        document.getElementById('submit').disabled = true;
+    } else {
+        document.getElementById('submit').disabled = false;
+    }
+}
