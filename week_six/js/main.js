a7f12413d243a0ca11a1d1ab2fae4808f1c007c0
michaelanell <mickimaree@gmail.com>
2022-06-02 19:43:49 -0600

    app progress

--- week_six/js/main.js
+++ week_six/js/main.js
@@ -6,30 +6,45 @@ let toDoItems = new Map();
 
 
 form.addEventListener('submit', addItem);
-
-
-
-function addItem() {
-    let i = toDoItems.size + 1;
-    toDoItems.set(i, item.value);
-    alert(item.value);
-    alert(toDoItems);
-    
-    createToDoList();
-    console.log('done');
-}
+document.querySelector('#to-do-menu-items').addEventListener('change', boxChecked);
 
 function createToDoList() {    
     console.log('got here');
+
     toDoItemsArray = Array.from(toDoItems);
     console.log(toDoItemsArray);
     
-    const itemListElement = document.getElementById("to-do-menu-items");
-    
-    for (i = 0; i < toDoItemsArray.length; i++) {
+    const itemList = document.getElementById("to-do-menu-items");
+
+    let i = toDoItems.size - 1;
+    checkBoxId = 'checkbox-' + i;
     const liElement = document.createElement("li");
-    liElement.innerHTML = toDoItemsArray[i][0];
-    }
-    }
+    const checkboxElement = document.createElement("input");
+    checkboxElement.setAttribute('type','checkbox');
+    checkboxElement.setAttribute('id', checkBoxId);
+
+
+    itemList.appendChild(liElement);
+    
+    liElement.innerHTML =  toDoItemsArray[i][1];
+    liElement.appendChild(checkboxElement);
+}
+
+function boxChecked(){
+    console.log('Got to the boxes');
+}
+
+
+function addItem(event) {
+    event.preventDefault();
+    let i = toDoItems.size + 1;
+    toDoItems.set(i, item.value);
+    console.log(item.value);
+    createToDoList();
+
+    item.value = '';
+    console.log('done');
+
+}
     
 
