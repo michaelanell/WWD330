91cf4fadc26b18b5ec0c229b00770ef350f2e822
michaelanell <mickimaree@gmail.com>
2022-06-25 15:13:05 -0600

    week 10

---
+++ week_ten/js/main.js
@@ -0,0 +1,20 @@
+function createMenuItems() {
+let weeklyTopics = new Map();
+
+weeklyTopics.set('Week One','week_one').set('Week Two', 'week_two').set('Week Three', 'week_three').set('Week Four', 'week_four').set('Week Five', 'week_five').set('Week Six','week_six').set('Week Seven','week_seven').set('Week Eight','week_eight');
+
+topicsMenu = Array.from(weeklyTopics);
+
+const topicsListElement = document.getElementById("week-menu-items");
+
+for (i = 0; i < topicsMenu.length; i++) {
+const liLink = document.createElement("a");
+liLink.setAttribute("href", `${topicsMenu[i][1]}`);
+const liElement = document.createElement("li");
+
+topicsListElement.appendChild(liLink);
+liLink.appendChild(liElement);
+
+liElement.innerHTML = topicsMenu[i][0];
+}
+}
